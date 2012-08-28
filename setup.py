cf4ba4833811919f175af2ac367efdfe21e85185
ben <bh0085@gmail.com>
2012-08-28 14:14:30 -0400

    first commit

---
+++ setup.py
@@ -0,0 +1,45 @@
+import os
+
+from setuptools import setup, find_packages
+
+here = os.path.abspath(os.path.dirname(__file__))
+README = open(os.path.join(here, 'README.txt')).read()
+CHANGES = open(os.path.join(here, 'CHANGES.txt')).read()
+
+requires = [
+    'pyramid',
+    'SQLAlchemy',
+    'transaction',
+    'pyramid_tm',
+    'pyramid_debugtoolbar',
+    'zope.sqlalchemy',
+    'waitress',
+    ]
+
+setup(name='brh',
+      version='0.0',
+      description='brh',
+      long_description=README + '\n\n' +  CHANGES,
+      classifiers=[
+        "Programming Language :: Python",
+        "Framework :: Pylons",
+        "Topic :: Internet :: WWW/HTTP",
+        "Topic :: Internet :: WWW/HTTP :: WSGI :: Application",
+        ],
+      author='',
+      author_email='',
+      url='',
+      keywords='web wsgi bfg pylons pyramid',
+      packages=find_packages(),
+      include_package_data=True,
+      zip_safe=False,
+      test_suite='brh',
+      install_requires=requires,
+      entry_points="""\
+      [paste.app_factory]
+      main = brh:main
+      [console_scripts]
+      initialize_brh_db = brh.scripts.initializedb:main
+      """,
+      )
+
