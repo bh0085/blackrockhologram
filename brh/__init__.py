be23eae9cf2c2841e2e8ded37d7956faeb265043
ben <bh0085@gmail.com>
2012-08-28 14:51:51 -0400

    landing page done

--- brh/__init__.py
+++ brh/__init__.py
@@ -9,8 +9,13 @@ def main(global_config, **settings):
     engine = engine_from_config(settings, 'sqlalchemy.')
     DBSession.configure(bind=engine)
     config = Configurator(settings=settings)
-    config.add_static_view('static', 'static', cache_max_age=3600)
     config.add_route('home', '/')
+
+    #static routing
+    config.add_static_view('/js/', 'public/js', cache_max_age=3600)
+    config.add_static_view('/css/', 'public/css', cache_max_age=3600)
+    config.add_static_view('/img/', 'public/img', cache_max_age=3600)
+
     config.scan()
     return config.make_wsgi_app()
 
