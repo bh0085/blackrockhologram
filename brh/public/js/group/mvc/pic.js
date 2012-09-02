6e9c9beffe338406437a01dd83d2ac0b86ddaee2
ben <bh0085@gmail.com>
2012-09-02 13:13:15 -0400

    basic shaping for the display format of the group landing page, first less/css and pic models.

---
+++ brh/public/js/group/mvc/pic.js
@@ -0,0 +1,73 @@
+var PBPic = Backbone.Model.extend({ 
+
+    defaults : function() {
+        return {
+            added: null,
+	    date_taken:null,
+            name: null,
+	    thumbs:null,
+	    has_thumbs: false};
+    },
+    initialize:function(args){
+	this._initialize_date();
+    }, 
+    url: function (data) {
+        var url = '/pbpics/' + this.id;
+        return url;
+    },
+    seconds:function(){
+	return Math.random() * 1000;
+    },
+    _initialize_date: function() {
+	var dt, settings, d;
+	dt =this.get('date_taken') ? Date.parse(this.get('date_taken')):Date.now();
+	d =this.get('date_taken') ? Date.parse(this.get('date_taken')):Date.now();
+	d.setSeconds(0);
+	d.setHours(0);
+	d.setMinutes(0);
+
+	settings = {
+	    date_string:[dt.getMonthName(),dt.getDay(),dt.getFullYear()].join(" "),
+	    day:Math.floor(dt/ (1000 * 3600 * 24)),
+	    age:dt/(1),
+	    datetime:dt,
+	    date:d
+	}
+	this.set(settings);
+    },
+});
+
+
+var Pic = Backbone.Model.extend({
+    seconds:function(){
+	return Math.random() * 1000;
+    },
+    url: function (data) {
+        var url = '/pics/' + this.id;
+        return url;
+    },
+})
+
+var PBPicView = Backbone.View.extend({
+    template: $("#pb-pic-view-template").html(),
+    tagName:"div",
+    className:"pb-pic-view",
+    render:function(){
+	var json = this.model.toJSON();
+	json.name = json.name?json.name:"untitled";
+	this.$el.html(Mustache.render(this.template, json));
+	return this;
+    }
+})
+
+
+var PicView = Backbone.View.extend({
+    template:$("#pic-view-template").html(),
+    tagName:"div",
+    className:"pb-pic-view",
+    render:function(){
+	var json= this.model.toJSON();
+	this.$el.html(Mustache.render(this.template, json));
+	return this;
+    }
+})
