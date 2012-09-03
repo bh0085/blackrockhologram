580ea88e9120b9aaaa3082f79063a495a501b432
Meelap Shah <meelapshah@gmail.com>
2012-09-03 18:29:04 -0400

    path

--- setup.sh
+++ setup.sh
@@ -20,11 +20,11 @@ venv/bin/easy_install paste || exit 1
 venv/bin/easy_install uwsgi || exit 1
 venv/bin/easy_install psycopg2 || exit 1
 
-venv/bin/python setup.py develop || exit 1
-
 ## Update DB server's IP in production.ini to point to master IP
 MASTER_IP="$1"
 cp production.ini.template production.ini
-sed -i -e "s/pb_dbuser:pictobin@.*:5432/pb_dbuser:pictobin@$MASTER_IP:5432/" $CWD/../production.ini || exit 1
-sed -i -e "s/brh_dbuser:blackrockhologram@.*:5433/brh_dbuser:blackrockhologram@$MASTER_IP:5432/" $CWD/../production.ini || exit 1
+sed -i -e "s/pb_dbuser:pictobin@.*:5432/pb_dbuser:pictobin@$MASTER_IP:5432/" $CWD/production.ini || exit 1
+sed -i -e "s/brh_dbuser:blackrockhologram@.*:5433/brh_dbuser:blackrockhologram@$MASTER_IP:5432/" $CWD/production.ini || exit 1
+
+venv/bin/python setup.py production || exit 1
 
